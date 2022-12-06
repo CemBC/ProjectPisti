d997d5d31aa7f7881acae1eb17c34279647b9887
CemBC <119740765+CemBC@users.noreply.github.com>
2022-12-06 22:09:51 +0300

    Catching problems
    trying to find problem of getting card method

--- GamePisti.java
+++ GamePisti.java
@@ -1,9 +1,12 @@
 public class GamePisti{
 	public static void main(String [] args){
 		Deck deck = new Deck();
+		deck.Shuffle();
+		deck.ShowDeck();
+		System.out.println("---------");
 		System.out.println(deck.getCard());
 		System.out.println(deck.getCard());
-		
+		deck.ShowDeck();
 		
 	}
 }
