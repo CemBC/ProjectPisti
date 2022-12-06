edaa9c183c428317ad83e6cfd1553cc293234d48
CemBC <119740765+CemBC@users.noreply.github.com>
2022-12-06 14:07:57 +0300

    Geting card
    Getting card from top of the deck, when you get a card from top of the deck that index will converted null to dont get that card again.

--- Deck.java
+++ Deck.java
@@ -7,6 +7,17 @@ public class Deck{
 		"♥A","♥2","♥3","♥4","♥5","♥6","♥7","♥8","♥9","♥10","♥J","♥Q","♥K",
 		"♦A","♦2","♦3","♦4","♦5","♦6","♦7","♦8","♦9","♦10","♦J","♦Q","♦K"};
 		this.deck = arr;
-		
+		}
+	public String getCard(){
+		String temporary;
+		for(int i = 0 ; i < deck.list ; i++) {
+			if(deck[i] != null) {
+				temporary = deck[i];
+				deck[i] = null;
+				return temporary;
+			}
+		}
 	}
+				
+				
 }
