5c080adc0b0c3b958361694c20f16eb6926b3dd5
CemBC <119740765+CemBC@users.noreply.github.com>
2022-12-06 14:24:54 +0300

    try to get null expression

--- GamePisti.java
+++ GamePisti.java
@@ -2,6 +2,7 @@ public class GamePisti{
 	public static void main(String [] args){
 		Deck deck = new Deck();
 		System.out.println(deck.getCard());
+		System.out.println(deck.getCard());
 		
 		
 	}
