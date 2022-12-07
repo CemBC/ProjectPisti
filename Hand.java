1c4bef55f781fd7342f6ae2d67df7f7466fb8a87
CemBC <119740765+CemBC@users.noreply.github.com>
2022-12-07 22:41:15 +0300

    GetFromHand
    getting card from hand

--- Hand.java
+++ Hand.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Hand{
 	private String[] hand;
 	
@@ -5,3 +6,11 @@ public class Hand{
 		String [] arr = new String[4];
 		this.hand = arr;
 	}
+	
+	public getFromHand(){
+		Scanner inp = new Scanner(System.in);
+		System.out.println("Which card do you want to play? Express it as 1,2,3 or 4 = ");
+		int index = inp.nextInt()-1;
+		return hand[index];
+	}
+		
