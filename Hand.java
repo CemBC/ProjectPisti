95b20ca0ab7b49000ebd55f6934e2dc35ea70dbb
CemBC <119740765+CemBC@users.noreply.github.com>
2022-12-07 22:47:54 +0300

    Adding
    Adding more methods and some fixes

--- Hand.java
+++ Hand.java
@@ -9,8 +9,28 @@ public class Hand{
 	
 	public getFromHand(){
 		Scanner inp = new Scanner(System.in);
-		System.out.println("Which card do you want to play? Express it as 1,2,3 or 4 = ");
-		int index = inp.nextInt()-1;
-		return hand[index];
+		while(true){
+			String temp;
+			System.out.println("Which card do you want to play? Express it as 1,2,3 or 4 = ");
+			int index = inp.nextInt()-1;
+			if(hand[index] == null){
+				System.out.println("You dont have a card on that position, try again");
+				continue;
+			}
+			temp = hand[index];
+			hand[index] = null;
+		}
+		return temp;
 	}
+	
+	public getToHand(String a, String b, String c, String d) {
+		hand[0] = a;
+		hand[1] = b;
+		hand[2] = c;
+		hand[3] = d;
+	}
+	
+}
+
+			
 		
